4e4a21718af3177bdb99fedb2deff788b4f6c46e
chenzhihui <chenzhihui244@msn.com>
2019-06-24 18:35:51 +0800

    update kvm

--- roles/kvm/templates/create-vm-ks.sh
+++ roles/kvm/templates/create-vm-ks.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+name=${1:-{{ vm_name }}}
+mkdir -p {{ kvm_path }}/${name}
+
 virt-install \
 	--name {{ vm_name }}-ks \
 	--os-type linux \
 	--os-variant "{{ os_variant }}" \
 	--memory {{ memory_mb }} \
-	--vcpus {{ vcpus }} \
-	--disk path={{ vdisk_path }}/disk.img,bus=virtio,size=32 \
+	--vcpus {{ vcpus }},cpuset={{ cpuset }} \
+    --numatune nodeset={{ nodeset }} \
+	--disk path={{ kvm_path }}/disk.img,bus=virtio,size=32 \
 	--network bridge={{ bridge }} \
 	--graphics vnc,listen=0.0.0.0,keymap=en-us \
 	--location {{ url_path }} \
--- roles/kvm/templates/create-vm.sh
+++ roles/kvm/templates/create-vm.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+name=${1:-{{ vm_name }}}
+mkdir -p {{ kvm_path }}/${name}
+
 virt-install \
-	--name {{ vm_name }} \
+	--name ${name} \
 	--os-type linux \
 	--os-variant "{{ os_variant }}" \
 	--memory {{ memory_mb }} \
-	--vcpus {{ vcpus }} \
-	--disk path={{ vdisk_path }}/disk.img,bus=virtio,size={{ vdisk_size }} \
+	--vcpus {{ vcpus }},cpuset={{ cpuset }} \
+    --numatune nodeset={{ nodeset }} \
+	--disk path={{ kvm_path }}/${name}/disk.img,bus=virtio,size={{ vdisk_size }} \
 	--network bridge={{ bridge }} \
 	--graphics vnc,listen=0.0.0.0,keymap=en-us \
 	--location {{ iso_path }} \
